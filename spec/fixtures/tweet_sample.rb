8c19a078b0ce2ceac90448a01330ba4e08c9a2a7
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-21 01:02:45 -0800

    Tweet model spec and fixture

--- spec/fixtures/tweet_sample.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'tweet_helper'
-
-describe Tweet do
-
-  let(:tweet_sample) { JSON.parse(File.read("#{Rails.root}/spec/fixtures/tweet_sample.json")) }
-
-end
--- spec/models/tweets_spec.rb
+++ spec/models/tweets_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
-require 'tweet_helper'
 
 describe Tweet do
+
+  let(:tweet_sample) { JSON.parse(File.read("#{Rails.root}/spec/fixtures/tweet_sample.json")) }
 
 end
