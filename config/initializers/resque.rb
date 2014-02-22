b82643bd50facc68e600d27a726dd569289b4a34
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-21 23:04:29 -0800

    Add password for redis connect

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,12 @@
 require 'resque/server'
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port)
+uri          = URI.parse(ENV["REDISTOGO_URL"])
+
+if Rails.env == 'production'
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port)
+end
+
 
 #Resque.logger.formatter = Resque::VerboseFormatter.new
