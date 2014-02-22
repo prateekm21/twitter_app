6743bf0e0ddd82c6c311bec6ea22580a0761c060
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-21 22:19:49 -0800

    Use blank in controller to check

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,8 +5,8 @@ class TweetsController < ApplicationController
   end
 
   def retrieve_tweets
-    if params[:geo_cordinates].length == 0 or params[:radius] == ""
-       response = {error: 'Missing cordinates or radius'}
+    if params[:geo_location].blank? or params[:radius].blank?
+       response  = {error: 'Missing cordinates or radius'}
        _status   = 400
     else
        response = Tweet.retrieve_tweets(params)
