1196a003a319619957ecb6eb6dc9ab6f28d8e1c5
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-21 22:29:47 -0800

    Fixed RSpec for controller

--- spec/controllers/tweets_controller_specs.rb
+++ spec/controllers/tweets_controller_specs.rb
@@ -13,7 +13,7 @@ describe TweetsController do
   describe "GET retrieve_tweets" do
 
     it "should return 400 status if params missing" do
-      get :retrieve_tweets , :geo_cordinates => [],  :radius => ""
+      get :retrieve_tweets , :geo_location => [],  :radius => ""
 
       response.status.should eql(400)
       JSON.parse(response.body)['error'].should  eql('Missing cordinates or radius')
@@ -21,7 +21,7 @@ describe TweetsController do
 
     it "should return desired info with correct params" do
       Tweet.should_receive(:retrieve_tweets).and_return({okay: true})
-      get :retrieve_tweets , :geo_cordinates => ["12.25542","13.545454"],  :radius => "10"
+      get :retrieve_tweets , :geo_location => ["12.25542","13.545454"],  :radius => "10"
 
       response.status.should eql(200)
       JSON.parse(response.body)['okay'].should be_true
