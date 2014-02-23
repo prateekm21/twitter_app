b6df535501dc03b92176da72b5b96a7587ffd47b
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-23 15:12:26 -0800

    Added tweet.js and seperate html and js

---
+++ app/assets/javascripts/tweet.js
@@ -0,0 +1,81 @@
+var validate = function(page_number,per_page){
+    var lat       = $("input#latitude").val(),
+        long      = $("input#longitude").val(),
+        rad       = $("input#radius").val(),
+        hash_tag  = $("input#hash_tag").val(),
+        imageBox  = $('div#tweetTemplate ul.result-items').empty(),
+        flag      = true;
+    input_validate    = [$('span#lat-help'),$('span#lon-help'), $('span#rad-help')]
+    $.each([lat,long,rad], function(i,e){
+        if (e == "")
+        {
+            input_validate[i].css('color', 'red');
+            flag = false;
+        }
+        else
+        {
+            input_validate[i].css('color', '');
+        }
+    });
+
+    if (flag)
+    {
+        renderTweets(lat,long,rad,hash_tag,page_number,per_page);
+    }
+
+};
+
+var renderTweets = function(lat,long,rad,hash_tag,page_number,per_page) {
+    var options   = {
+        geo_location: [lat,long],
+        radius:       rad,
+        hash_tag:     hash_tag,
+        page:         page_number,
+        per_page:     per_page
+    }
+    _api      = "/tweets/retrieve_tweets",
+        error     = $('div.controls p.help-block span#error').empty(),
+        targetEle = $(document.body).find('#tweetTemplate ul.result-items');
+    map.setCenter(new google.maps.LatLng(lat,long));
+    $.ajax({
+        type: "GET",
+        url:  _api,
+        data: options
+    }).success(function(data) {
+            showResult(data);
+        });
+};
+
+var showPagination = function(total, current){
+    window.current = current;
+    var panel = $("#pagination_panel");
+    var pages = Math.ceil(total/20);
+    panel.hide();
+    if( pages > 0){
+        panel.show();
+        $("#total-pg").text(pages);
+        $("#curr-pg").text(current);
+        $("#prev-btn").show();
+        $("#next-btn").show();
+        if(current == 1){
+            $("#prev-btn").hide();
+        }
+        if(current == pages){
+            $("#next-btn").hide();
+        }
+        bindPaginationBtns();
+    }
+};
+
+var bindPaginationBtns = function(){
+  if(!window.binded){
+      window.binded = 1;
+      $("#prev-btn").bind('click', function(){
+          validate(window.current-1,20);
+
+      });
+      $("#next-btn").bind('click', function(){
+          validate(window.current+1,20);
+      });
+  }
+};
