38305a0a82b0d2bc7106673fa3629e0e8e3207dd
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-27 21:20:20 -0800

    Added some controller specs

--- spec/controllers/tweets_controller_specs.rb
+++ spec/controllers/tweets_controller_specs.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe TweetsController do
+  before(:all) do
+    @location = ["12.25542","13.545454"]
+    @radius   = 10
+  end  
 
   describe "GET index" do
-
+    
     it "should return success response" do
       get :index
       response.should be_success
@@ -12,7 +16,7 @@ describe TweetsController do
 
   describe "GET retrieve_tweets" do
 
-    it "should return 400 status if params missing" do
+    it "should return 422 status if params missing" do
       get :retrieve_tweets , :geo_location => [],  :radius => ""
 
       response.status.should eql(422)
@@ -21,10 +25,19 @@ describe TweetsController do
 
     it "should return desired info with correct params" do
       Tweet.should_receive(:retrive_tweets).and_return({okay: true})
-      get :retrieve_tweets , :geo_location => ["12.25542","13.545454"],  :radius => "10"
+      get :retrieve_tweets , :geo_location => @location,  :radius => @radius
 
       response.status.should eql(200)
       JSON.parse(response.body)['okay'].should be_true
     end
+
+    it "should return desired info with page in params" do
+      Tweet.should_receive(:retrive_tweets).and_return({okay: true})
+      get :retrieve_tweets , :geo_location => @location,  :radius => @radius,  :page => 1, :per_page => 1
+
+      response.status.should eql(200)
+      JSON.parse(response.body)['okay'].should be_true
+    end
+
   end
 end
